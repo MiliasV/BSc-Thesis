617add5c035d721bd8ff7bb9bcb2b6b1ca89c8de
MiliasV <milias.vasilis@gmail.com>
2016-05-24 13:43:57 +0300

    last version

--- step4/fabfile.py
+++ step4/fabfile.py
@@ -13,15 +13,17 @@ from ast  import literal_eval
 env.disable_known_hosts=True
 env.warn_only=True
 #env.hosts=['mininet@192.168.56.102','mininet@192.168.56.101','mininet@83.212.102.97']
-env.hosts=['mininet@147.102.13.69','mininet@83.212.102.97','mininet@147.102.13.61']
+env.hosts=['mininet@83.212.108.121','mininet@83.212.102.97','mininet@83.212.108.122','mininet@83.212.108.123','mininet@83.212.108.124']
 
 #env.hosts = ["mininet@83.212.102.97","mininet@147.102.13.61","mininet@192.168.56.103"]
 
 #env.passwords = {'mininet@192.168.56.101:22': 'mininet', 'mininet@192.168.56.102:22': 'mininet','mininet@192.168.56.103:22' : 'mininet','mininet@83.212.102.97:22': 'byslaf366'}
-env.passwords = {'mininet@83.212.102.97:22': 'byslaf366', 'mininet@147.102.13.61:22': 'mininet','mininet@147.102.13.69:22' : 'mininet'}
+env.passwords = {'mininet@83.212.102.97:22': 'byslaf366','mininet@83.212.108.121:22': 'byslaf366','mininet@83.212.108.122:22': 'byslaf366','mininet@83.212.108.123:22': 'byslaf366','mininet@83.212.108.124:22': 'byslaf366'}
+                 
 
 #ips=['83.212.102.97','192.168.56.102','192.168.56.101']
-ips=['83.212.102.97','147.102.13.61','147.102.13.69']
+ips=['83.212.102.97','83.212.108.121','83.212.108.122','83.212.108.123','83.212.108.124']
+     
      
 #'83.212.102.97',
      
@@ -30,34 +32,24 @@ ips=['83.212.102.97','147.102.13.61','147.102.13.69']
 def showOvs():
         run('sudo ovs-vsctl show')
 
+@hosts('mininet@83.212.102.123')
+def victimServer():
+    run('sudo mn -c')
+    run('mkdir -p testing')
+    put('edges_mininet.txt','~/testing/')
+    put('min_builder.py','~/testing/')
+    put('allMh.txt','~/testing/')
+    print '############################ %s ################'%env.host
+    run('cd testing && sudo python min_builder.py %s edges_mininet.txt allMh.txt &' %env.host , pty=False)
+
 @parallel
 def runOneLs():
     run('ls')
 
-
 @parallel
+@hosts('mininet@83.212.108.122','mininet@83.212.108.121','mininet@83.212.102.97','mininet@83.212.108.123')
+#@hosts('mininet@83.212.108.121')
 def connection():
-	#let's you interact with the remote VM from terminal
-	#open_shell()
-	#run('sudo mn')
-	#with cd( '~/mininet/custom/'):
-	#	run('ls')
-	#put('adjToNetwork.py','~/')
-	#put('exam.csv','~/')
-	#put('~/USC-NSL-miniNExT-75c2781')
-        # Simplest form:
-        #environment = prompt('Please specify target environment: ')
-        # With default, and storing as env.dish:
-        #prompt('Specify favorite dish: ', 'dish', default='spam & eggs')
-        #print("%s"%env.dish)
-        #open_shell()
-        #with hide('output'):
-        #with
-        #run('sudo mn -c')
-        #put ('script.py','~/')
-        #put ('list.txt','~/')
-        #run('python script.py list.txt %s' %env.host)
-	#run('sudo dhclient eth1')
         run('sudo mn -c')
         run('mkdir -p testing')
         put('edges_mininet.txt','~/testing/')
@@ -65,15 +57,13 @@ def connection():
         put('allMh.txt','~/testing/')
         print '############################ %s ################'%env.host
         run('cd testing && sudo python min_builder.py %s edges_mininet.txt allMh.txt' %env.host , pty=False)
-        #with settings(warn_only=True):
-            #run('nohup sudo python  adjToNetwork.py exam.csv ')
-            #run('disown')
 
 if __name__=='__main__':
    
     f=str(sys.argv[1])
     num=int(sys.argv[2])
-    os.system("python from_paths_to_vms.py %(file)s %(number)s" %{"file":f, "number":num})
+    mH=sys.argv[3]
+    os.system("python from_paths_to_vms.py %(file)s %(number)s %(mh)s" %{"file":f, "number":num,"mh":mH})
     f1=open("new_edges.txt",'r')
     #f1=open(sys.argv[3])
     line=f1.readline()
@@ -90,11 +80,5 @@ if __name__=='__main__':
         print>>file, item
     file.close()
     f1.close()
+    #execute(victimServer)
     execute(connection)
-    """
-    print " "
-    print '###############################'
-    print l
-    """
-    """
-    """
